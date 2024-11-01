c432facc96031bce33c2770de6840ab07a319be1
ALUNO <ALUNO@senaiescola.fsa>
2024-11-01 09:15:37 -0300

    meu primeiro commit

---
+++ lib/teste.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: QuizPage(),
+    );
+  }
+}
